4f5b35a3ffc18eb78476dcb0dfe8860cf7bf0a07
brndn-do <dobrandon05@gmail.com>
2024-07-08 21:51:14 -0500

    Finish project

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 "use strict";
 
-let a;
-let b;
-let operator;
-
 function add(a, b) {
-    return a + b;
+    return parseFloat(a) + parseFloat(b);
 }
 
 function subtract(a, b) {
@@ -17,6 +13,9 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if (b === 0) {
+        return "ERROR"
+    }
     return a / b;
 }
 
@@ -32,3 +31,67 @@ function operate(a, b, operator) {
             return divide(a, b);
     }
 }
+
+let a = 0;
+let b = "";
+let operator = "";
+const display = document.querySelector("#display");
+
+function populateDisplay(val) {
+    if (val === "ERROR") {
+        display.textContent = val;
+    }
+    else {
+        display.textContent = Math.round(10**13 * val) / 10 ** 13;
+    }
+    return val;
+}
+
+function processInput(val) {
+    let nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    if (val === "C") {
+        a = 0;
+        b = "";
+        operator = "";
+        populateDisplay("");
+    }
+    else if (val === "=") {
+        if ((a !== 0 && b && operator)) {
+            a = populateDisplay(operate(a, b, operator));
+            if (a === "ERROR") {
+                a = 0;
+                operator = "";
+            }
+            b = "";
+        }
+    }
+    else if (nums.includes(val)) {
+        if (!operator) {
+            a = parseInt(`${a}` + val);
+            populateDisplay(a);
+        }
+        else {
+            b += val;
+            populateDisplay(b);
+        }
+    }
+    else {
+        if (b) {
+            a = populateDisplay(operate(a, b, operator));
+            if (a === "ERROR") {
+                a = 0;
+                operator = "";
+            }
+            b = "";
+        }
+        operator = val;
+    }
+}
+
+const numbers = document.querySelectorAll("button");
+numbers.forEach(button => {
+    button.addEventListener("click", event => {
+        processInput(event.target.id);
+    });
+});
+
